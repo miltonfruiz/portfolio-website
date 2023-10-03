02e0cd3d5a3f36132c4238fa01f4e02e2d02a8b6
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-10-03 20:42:34 -0300

    Agregado foto perfil en banner

--- frontend/src/components/Banner/index.jsx
+++ frontend/src/components/Banner/index.jsx
@@ -4,8 +4,14 @@ import "./style.css";
 export default function Banner() {
   return (
     <>
-      <div className="row justify-content-center d-flex align-items-center navContainer mt-5">
-        <div className="banner col-9"></div>
+      <div className="row justify-content-center d-flex align-items-center navContainer mt-5 mb-5">
+        <div className="banner col-9 d-flex align-items-end">
+          <img
+            className="imgProfile"
+            src="public/images/perfil.jpg"
+            alt="profile-img"
+          />
+        </div>
       </div>
     </>
   );
